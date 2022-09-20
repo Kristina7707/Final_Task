c4ee4404a339f758d8c1ca3bd961f5a8bc77c3fb
Kristina <40335@mail.ru>
2022-09-20 10:57:23 +0300

    add void SecondArray

--- task/Program.cs
+++ task/Program.cs
@@ -10,3 +10,15 @@
 
 string[] array1 = new string[5] {"Hello", "1234", "-2", "world", "2"};
     string[] array2 = new string[array1.Length];
+    void SecondArray(string[] array1, string[] array2)
+    {
+        int count = 0;
+        for (int i = 0; i < array1.Length; i++)
+        {
+        if(array1[i].Length <= 3)
+            {
+            array2[count] = array1[i];
+            count++;
+            }
+        }
+    }
